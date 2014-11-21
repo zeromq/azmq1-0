896cb5382d7cccd4cee181ac1675d57cdfb4abd4
Andrey Upadyshev <oliora@gmail.com>
2014-11-21 14:10:30 +0100

    Fix ZeroMQ finder to work on Windows (version extracting is not impmelened yet)

--- config/FindZeroMQ.cmake
+++ config/FindZeroMQ.cmake
@@ -19,23 +19,27 @@ find_path(ZeroMQ_INCLUDE_DIRS NAMES zmq.h HINTS ${_ZeroMQ_ROOT}/include)
 
 set(_ZeroMQ_H ${ZeroMQ_INCLUDE_DIRS}/zmq.h)
 
-find_library(ZeroMQ_LIBRARIES NAMES zmq HINTS ${_ZeroMQ_ROOT}/lib)
+find_library(ZeroMQ_LIBRARIES NAMES zmq libzmq HINTS ${_ZeroMQ_ROOT}/lib)
 
-function(_zmqver_EXTRACT _ZeroMQ_VER_COMPONENT _ZeroMQ_VER_OUTPUT)
-    execute_process(
-        COMMAND grep "#define ${_ZMQ_VER_COMPONENT}"
-        COMMAND cut -d\  -f3
-        RESULT_VARIABLE _zmqver_RESULT
-        OUTPUT_VARIABLE _zmqver_OUTPUT
-        INPUT_FILE ${_ZeroMQ_H}
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-    set(${_ZeroMQ_VER_OUTPUT} ${_zmqver_OUTPUT} PARENT_SCOPE)
-endfunction()
+# TODO: implement version extracting for Windows
+if (NOT ${CMAKE_CXX_PLATFORM_ID} STREQUAL "Windows")
+    function(_zmqver_EXTRACT _ZeroMQ_VER_COMPONENT _ZeroMQ_VER_OUTPUT)
+        execute_process(
+            COMMAND grep "#define ${_ZMQ_VER_COMPONENT}"
+            COMMAND cut -d\  -f3
+            RESULT_VARIABLE _zmqver_RESULT
+            OUTPUT_VARIABLE _zmqver_OUTPUT
+            INPUT_FILE ${_ZeroMQ_H}
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        set(${_ZeroMQ_VER_OUTPUT} ${_zmqver_OUTPUT} PARENT_SCOPE)
+    endfunction()
 
-_zmqver_EXTRACT("ZMQ_VERSION_MAJOR" ZeroMQ_VERSION_MAJOR)
-_zmqver_EXTRACT("ZMQ_VERSION_MINOR" ZeroMQ_VERSION_MINOR)
+    _zmqver_EXTRACT("ZMQ_VERSION_MAJOR" ZeroMQ_VERSION_MAJOR)
+    _zmqver_EXTRACT("ZMQ_VERSION_MINOR" ZeroMQ_VERSION_MINOR)
 
-set(ZeroMQ_FIND_VERSION_EXACT "${ZMQ_VERSION_MAJOR}.${ZMQ_VERSION_MINOR}")
+    set(ZeroMQ_FIND_VERSION_EXACT "${ZMQ_VERSION_MAJOR}.${ZMQ_VERSION_MINOR}")
+endif()
+
 find_package_handle_standard_args(ZeroMQ FOUND_VAR ZeroMQ_FOUND
                                       REQUIRED_VARS ZeroMQ_INCLUDE_DIRS ZeroMQ_LIBRARIES
                                       VERSION_VAR ZeroMQ_VERSION)
