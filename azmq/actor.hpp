f5ed9effadc5ad548eafe5493da3a28c123f4f2e
Thomas Rodgers <rodgert@twrodgers.com>
2014-12-19 12:24:14 -0600

    Fix include guards

--- azmq/actor.hpp
+++ azmq/actor.hpp
@@ -6,8 +6,8 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 */
-#ifndef AZMQ_THREAD_HPP_
-#define AZMQ_THREAD_HPP_
+#ifndef AZMQ_ACTOR_HPP_
+#define AZMQ_ACTOR_HPP_
 
 #include "socket.hpp"
 #include "detail/actor_service.hpp"
