cf698750fb94079a0d77d3c08639560db7203677
Thomas Rodgers <trodgers@drw.com>
2015-04-02 17:10:00 -0500

    Adjust configs based on feedback

--- azmq/detail/config.hpp
+++ azmq/detail/config.hpp
@@ -9,12 +9,21 @@
 #ifndef AZMQ_DETAIL_CONFIG_HPP_
 #define AZMQ_DETAIL_CONFIG_HPP_
 
-#if !defined BOOST_NO_CXX11_INLINE_NAMESPACES
-    #define AZMQ_V1_INLINE_NAMESPACE_BEGIN inline namespace v1 {
-    #define AZMQ_V1_INLINE_NAMESPACE_END }
-#else
-    #define AZMQ_V1_INLINE_NAMESPACE_BEGIN
-    #define AZMQ_V1_INLINE_NAMESPACE_END
-#endif
+#if !defined AZMQ_USE_STANDALONE_ASIO
+#   include <boost/config.hpp>
+#   if !defined BOOST_NO_CXX11_INLINE_NAMESPACES
+#       define AZMQ_V1_INLINE_NAMESPACE_BEGIN inline namespace v1 {
+#       define AZMQ_V1_INLINE_NAMESPACE_END }
+#   else
+#       define AZMQ_V1_INLINE_NAMESPACE_BEGIN
+#       define AZMQ_V1_INLINE_NAMESPACE_END
+#   endif
+#else // AZMQ_USE_STANDALONG_ASIO
+// Assume a competent C++11 implementation
+#       define ASIO_STANDALONE 1
+#       define AZMQ_V1_INLINE_NAMESPACE_BEGIN inline namespace v1 {
+#       define AZMQ_V1_INLINE_NAMESPACE_END }
+#endif //!defined AZMQ_USE_STANDALONE_ASIO
+
 
 #endif // AZMQ_DETAIL_CONFIG_HPP_
--- azmq/detail/config/thread.hpp
+++ azmq/detail/config/thread.hpp
@@ -9,18 +9,16 @@
 #ifndef AZMQ_DETAIL_CONFIG_THREAD_HPP_
 #define AZMQ_DETAIL_CONFIG_THREAD_HPP_
 
-#if !defined(AZMQ_HAS_STD_THREAD)
 #if !defined(AZMQ_DISABLE_STD_THREAD)
-#include <thread>
-#define AZMQ_HAS_STD_THREAD 1
+#   include <thread>
+#   define AZMQ_HAS_STD_THREAD 1
     namespace azmq { namespace detail {
         using thread_t = std::thread;
     } }
 #else // defined(AZMQ_DISABLE_STD_THREAD)
-#include <boost/thread/thread.hpp>
+#   include <boost/thread/thread.hpp>
     namespace azmq { namespace detail {
         using thread_t = boost::thread;
     } }
-#endif // !defined(AZMQ_DISABLE_STD_THREAD)
+#   endif // !defined(AZMQ_DISABLE_STD_THREAD)
 #endif // !defined(AZMQ_HAS_STD_THREAD)
-#endif // AZMQ_DETAIL_CONFIG_THREAD_HPP_
